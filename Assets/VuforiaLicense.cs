11f050b548cec1e40cad4865c98906860a6bfb0e
Anki Prawira <ankiprawira@gmail.com>
2023-06-04 22:49:08 +0700

    added quiz start menu and credits

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVg5Z0ZjNy8vLy8vQUFBQm1RSi9wZDRRRDBrZXRUeS9VL2VZanI0M1pDd0RDSmRaS21LbHUyTWZZV0xidmFRRHhLSHNSYjRZTEJRck5hMEtMWndYVTg5VzkyZTl5TTFjTDdNMlAvUUk0N0ZwUGtscFowc3hMWC8xcmEyOWZPSmlSbEthTzh3MGhtajVDODNhS0t0cEhJenRqMWk3aktVQU1SRUk1akE1WUxhc2JJQXBwZDFjMlFLMXd1WU1aWk9kZlFvaXd6aFRJdDhWeVE2RGhQSGJTWjh0ekdEekw1U3gyaHoxV2hocmx4em1DTW1kZ2pYMG1wUjM3c1ZaRHVucE9TNnhEdXlLcEJMeTY5UHVybHJrUThTeHFjUUdGK2FvZTBDVzl1TWRjLzFQd1JTMS9IYy9USEhrQkx0alpMTVBUVWowZHhHc09BcGpZRUpLZjhISm91OHRQU0U2NjlSRXlySENlWlVacWUwT2IzNXgwa0FXK0JLb2JLby8=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
